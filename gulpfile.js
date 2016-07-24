cd217f284c83978032b8660f49eea7067b486665
Liem Pham <pham.ducliem@gmail.com>
2016-07-24 16:15:56 +0100

    Add comments and a svg compression task

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ var svgmin = require('gulp-svgmin');
 var path = require('path');
 var del = require('del');   // to delete folders & files
 
+/**
+ * Compress each svg files with svgmin and merge them into a spritesheet
+ */
 gulp.task('svgstore', function () {
     return gulp
         .src(['app/icons/*.svg']) //Source folder where to find your svg
@@ -19,9 +22,21 @@ gulp.task('svgstore', function () {
             };
         }))
         .pipe(svgstore({ inlineSvg: true }))
-        .pipe(gulp.dest('dist/icons')); //Destination folder where to find your compressed svg
+        .pipe(gulp.dest('dist/icons/spritesheet')); //Destination folder where to find your compressed svg
 });
 
+/**
+ * Compress each svg files with svgmin
+ */
+gulp.task('svgmin', function () {
+    return gulp.src('app/icons/*.svg')
+        .pipe(svgmin())
+        .pipe(gulp.dest('dist/icons/compressed'));
+});
+
+/**
+ * Delete the dist folder to have a clean generation of svgs
+ */
 gulp.task('clean', function() {
     return del.sync('dist');
 });
